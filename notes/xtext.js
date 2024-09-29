0a4cff614c079fd9c0ed652735cb01ad7db8f071
step135 <47241203+step135@users.noreply.github.com>
2024-09-29 15:52:30 +0200

    Update xtext.js

--- notes/xtext.js
+++ notes/xtext.js
@@ -94,6 +94,14 @@ xtext = {
             '<l onclick="clicked()">$1$4</l>'
         );
     },
+    shorten_links : function (s) {
+        s = s.replace(/(ali)(express\.com)/g,"$1<nv>$2</nv>");
+        s = s.replace(/(f)(ace)(b)(ook\.com)/g,"$1<nv>$2</nv>$3<nv>$4</nv>");
+        s = s.replace(/>(https:\/\/(www\.|))/g,"><nv>$1</nv>");
+        s = s.replace(/(\.com\/item)\//g,"<nv>$1</nv>/");
+        s = s.replace(/(\.html)</g,"<nv>$1</nv><");
+        return s;
+    },
     style_fixed: function (f) {
         return f.replace(/frame(@\/[0-9]+\/@)/, '<div class="frame">$1</div>');
     },
@@ -266,6 +274,7 @@ xtext = {
             si[i] = this.price(si[i]);
             console.log("si[i]", si[i]);
             si[i] = this.activate_links(si[i]);
+            si[i] = this.shorten_links(si[i]);
             if (si[i][0] == "#") {
                 if (si[i][1] == "#")
                     si[i] = "<h4>" + si[i].replace(/##\s*/, "") + "</h4>";
