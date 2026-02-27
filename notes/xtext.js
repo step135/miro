c73ee13fb0e7824ea4c18279fa05a3c53c25baf0
step135 <47241203+step135@users.noreply.github.com>
2026-02-27 20:34:01 +0100

    Fix regex to capture full URL in xtext.js

--- notes/xtext.js
+++ notes/xtext.js
@@ -375,7 +375,7 @@ xtext = {
             .replace(/\r/g, "")
             .replace(/^\s+|\s+$/g, "")
             .replace(
-                /http\S+[?&]url=([^\s&]+)/g,
+                /http\S+[?&]url=([^\s&]+)[^.,:?\s]*/g,
                 function (a, a1) {
                     return decodeURIComponent(a1);
                 }
